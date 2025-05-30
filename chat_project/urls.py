edba9f3262334dc8161e3767fe2bd29bdc9e9fa9
Lucifer <luciferms616@gmail.com>
2025-05-29 19:08:28 -0500

    Fix favicon

--- chat_project/urls.py
+++ chat_project/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('', include('chat.urls')),
 ]
 
-# if settings.DEBUG:
-    # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
