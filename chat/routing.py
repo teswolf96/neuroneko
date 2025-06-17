648d2372803327b21bcd1f259c808379f76c4c21
tes0718 <tshaw@spp.org>
2025-06-17 13:10:12 -0500

    Fix websocket nonsense

--- chat/routing.py
+++ chat/routing.py
@@ -4,5 +4,5 @@ from . import consumers
 websocket_urlpatterns = [
     # Old route, can be removed or commented out if ChatConsumer is no longer used
     # re_path(r'ws/chat/$', consumers.ChatConsumer.as_asgi()), 
-    re_path(r'ws/chat/(?P<chat_id>[0-9a-f-]+)/$', consumers.StreamingChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<chat_id>\d+)/$', consumers.StreamingChatConsumer.as_asgi()), # Changed regex to \d+ for integer IDs
 ]
